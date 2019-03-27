9caa00d5e608f72432c343090af172ebbcd16584
839936364 <839936364@qq.com>
2019-03-27 23:36:14 +0800

    acly 02

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import Vue from "vue";
-import App from "./App.vue";
-// Vue.config.productionTip = false
 //1.导入vue-router包
-import VueRouter from "vue-router";
-//2.手动安装VueRouter
-Vue.use(VueRouter);
-import router from "./router.js";
+import router from "./router";
+
+import App from "./App.vue";
+Vue.config.productionTip = false
+
+import './assets/statics/site/css/style.css'
+// 安装axios
+import axios from "axios";
+Vue.prototype.$http = axios;
+
+//导入格式化时间的插件
+import moment from "moment";
+//定义全局过滤器
+Vue.filter("dateFormat", function(dataStr, pattern = "YYYY-MM-DD") {
+  return moment(dataStr).format(pattern);
+});
+Vue.filter("FormatDate", function(dataStr, pattern = "YYYY-MM-DD HH:MM:SS") {
+  return moment(dataStr).format(pattern);
+});
+// 引入组件
+// import ElementUI from 'element-ui'
+
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// Vue.use(ElementUI)
+
 new Vue({
   render: h => h(App),
   router
--- src/router.js
+++ src/router.js
@@ -1,7 +1,28 @@
+import Vue from "vue";
 import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+import index from './components/index.vue'
+import car from './components/car.vue'
+import detail from './components/detail.vue'
+import member from './components/member.vue'
+import userCenter from './components/userCenter.vue'
+import userOrder from './components/userOrder.vue'
+import userDetail from './components/userDetail.vue'
+import Settlement from './components/settlement.vue'
+
+
 const router = new VueRouter({
   routes: [
-
+    { path: "/", redirect: "/index" },
+    { path: "/index", component: index },
+    { path: "/car", component: car },
+    { path: "/settlement", component: Settlement },
+    { path: "/detail/:id", component: detail },
+    { path: "/member", component: member },
+    { path: "/user/order", component: userOrder },
+    { path: "/user/center", component: userCenter },
+    { path: "/user/detail", component: userDetail },
   ]
 });
 export default router;
