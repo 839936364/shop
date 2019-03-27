f3463a33c54c10909cb47b5f4c0d1c65483a0e1e
839936364 <839936364@qq.com>
2019-03-27 10:14:37 +0800

    shop 初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
+// Vue.config.productionTip = false
+//1.导入vue-router包
+import VueRouter from "vue-router";
+//2.手动安装VueRouter
+Vue.use(VueRouter);
+import router from "./router.js";
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app");
